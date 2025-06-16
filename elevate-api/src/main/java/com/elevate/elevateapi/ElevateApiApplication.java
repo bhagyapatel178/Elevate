8443fb0a8ede5ada83975d4193b2aecc55827df3
bhagyapatel178 <bhagyapatel178@gmail.com>
2025-06-16 23:43:51 +0100

    springboot skeleton setup with postgres connection

---
+++ elevate-api/src/main/java/com/elevate/elevateapi/ElevateApiApplication.java
@@ -0,0 +1,13 @@
+package com.elevate.elevateapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElevateApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElevateApiApplication.class, args);
+    }
+
+}
---
+++ elevate-api/src/test/java/com/elevate/elevateapi/ElevateApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elevate.elevateapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElevateApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
