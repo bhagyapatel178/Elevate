e89d276fa57897c072e623632a217774e76f540e
bhagyapatel178 <bhagyapatel178@gmail.com>
2025-06-17 21:27:20 +0100

    feat: user entity, added more fields and a constructor

--- elevate-api/src/main/java/com/elevate/elevateapi/entity/User.java
+++ elevate-api/src/main/java/com/elevate/elevateapi/entity/User.java
@@ -2,25 +2,50 @@ package com.elevate.elevateapi.entity;
 
 import jakarta.persistence.*;
 
+import java.time.LocalDateTime;
+
 
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true)
     private String username; // display name
+
+    @Column(unique = true)
     private String email;
+
     private String password; // hash
 
     @Enumerated(EnumType.STRING)
     private GenderType gender;
+
     private int age;
+
     @Enumerated(EnumType.STRING)
     private MeasurementUnitSystem  preferredUnitSystem; // cm,kg or ft,inches,lb
+
     private double heightCm; // store in cm
     private double weightKg; // store in kg
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
 
     public enum GenderType {
         MALE,
@@ -33,5 +58,7 @@ public class User {
         METRIC,
         IMPERIAL
     }
+
+    public User() {}
 
 }
