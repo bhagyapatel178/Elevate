be6f372064e9160cf74d7b16b4c85dc02879747d
bhagyapatel178 <bhagyapatel178@gmail.com>
2025-06-17 23:41:43 +0100

    feat: progress log entity

---
+++ elevate-api/src/main/java/com/elevate/elevateapi/entity/ProgressLog.java
@@ -0,0 +1,32 @@
+package com.elevate.elevateapi.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "progress_log")
+public class ProgressLog {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private LiftType liftType;
+
+    private LocalDate date;
+    private String variation; // eg weighted for dips or inclined for db press
+    private double weight;
+    private int reps;
+
+    public enum LiftType{
+        BENCH_PRESS,
+        SQUAT,
+        DEADLIFT,
+        DUMBELL_PRESS,
+        PULL_UPS,
+        DIPS
+    }
+
+}
