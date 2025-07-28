5ce55a53645449b86ae1b54ad1b9a962f3009ba9
bhagyapatel178 <bhagyapatel178@gmail.com>
2025-07-29 00:15:24 +0100

    feat: logs appear in calender and in home page

--- elevate-client/src/app/pages/profile/profile.component.ts
+++ elevate-client/src/app/pages/profile/profile.component.ts
@@ -3,30 +3,14 @@ import {HttpClient} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
 import {CommonModule} from '@angular/common';
 
-import { CalendarModule, CalendarEvent, CalendarView} from 'angular-calendar';
-
-interface ProgressLog {
-  id: number;
-  liftType: string;
-  variation: string;
-  weight: number;
-  reps: number;
-  date: string;
-  userId: number;
-}
 
 @Component({
   selector: 'app-profile',
-  imports: [FormsModule,CommonModule,CalendarModule],
+  imports: [FormsModule,CommonModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent implements  OnInit{
-
-
-  view: CalendarView = CalendarView.Month;
-  viewDate: Date = new Date();
-  events: CalendarEvent[] = [];
 
   isEditing = false;
 
@@ -50,24 +34,6 @@ export class ProfileComponent implements  OnInit{
     weight: 0
   };
 
-  newLift = {
-    liftType: '',
-    variation: ''
-  };
-
-  sets: { weight: number | null, reps: number | null }[] = [
-    { weight: null, reps: null }
-  ];
-
-  liftOptions = [
-    { label: 'Bench Press', value: 'BENCH_PRESS' },
-    { label: 'Squat', value: 'SQUAT' },
-    { label: 'Deadlift', value: 'DEADLIFT' },
-    { label: 'Dumbbell Press', value: 'DUMBBELL_PRESS' },
-    { label: 'Pull Ups', value: 'PULL_UPS' },
-    { label: 'Dips', value: 'DIPS' }
-  ];
-
   genderOptions = [
     { label: 'Male', value: 'MALE' },
     { label: 'Female', value: 'FEMALE' },
@@ -80,7 +46,7 @@ export class ProfileComponent implements  OnInit{
     { label: 'Imperial (lbs, in)', value: 'IMPERIAL' }
   ];
 
-  progressLogs: ProgressLog[] = [];
+  // progressLogs: ProgressLog[] = [];
 
   constructor(private http: HttpClient) {}
 
@@ -91,45 +57,6 @@ export class ProfileComponent implements  OnInit{
         this.updatedProfile = {...res}; // clones data
       },
       error: err => alert('Error registering user')
-    })
-    this.fetchLogs();
-  }
-
-  getLiftOutput(liftType: string){
-    if (liftType == 'BENCH_PRESS'){return 'Bench Press'}
-    if (liftType == 'SQUAT'){return 'Squat'}
-    if (liftType == 'DEADLIFT'){return 'Deadlift'}
-    if (liftType == 'DUMBBELL_PRESS'){return 'Dumbbell Press'}
-    if (liftType == 'PULL_UPS'){return 'Pull Ups'}
-    if (liftType == 'DIPS'){return 'Dips'}
-    return ''
-  }
-
-  createLog(){
-    const logsToSend = this.sets.map(set => ({
-      liftType: this.newLift.liftType,
-      variation: this.newLift.variation,
-      weight: set.weight,
-      reps: set.reps
-    }));
-
-    logsToSend.forEach(log => {
-      this.http.post('api/progress-logs', log).subscribe({
-        next: () => this.fetchLogs(),
-        error: err => alert(err.error?.message)
-      });
-    });
-    this.newLift = { liftType: '', variation: '' };
-    this.sets = [{ weight: null, reps: null }];
-  }
-
-
-
-  fetchLogs(){
-    this.http.get<any>('/api/users/me/logs').subscribe({
-      next: (res: any) => this.progressLogs = res,
-      error: err => alert('Error fetching logs'),
-
     })
   }
 
@@ -156,6 +83,4 @@ export class ProfileComponent implements  OnInit{
     return match ? match.label : value;
   }
 
-
 }
-
