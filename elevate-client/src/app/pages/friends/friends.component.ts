3c3c3eb9416967fc93c539bdb326d96711481d1b
bhagyapatel178 <bhagyapatel178@gmail.com>
2025-07-30 19:04:11 +0100

    feat: search for specific friend

--- elevate-client/src/app/pages/friends/friends.component.ts
+++ elevate-client/src/app/pages/friends/friends.component.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import {Component,ChangeDetectionStrategy, OnInit} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+
+interface UserSearchResult {
+  id: number;
+  username: string;
+}
 
 @Component({
   selector: 'app-friends',
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './friends.component.html',
-  styleUrl: './friends.component.scss'
+  styleUrl: './friends.component.scss',
 })
-export class FriendsComponent {
+export class FriendsComponent{
+
+  friendToSearch = ''
+
+  result?: UserSearchResult;
+  error = '';
+
+  constructor(private http: HttpClient) {}
+
+  findUser(): void {
+    this.error = '';
+    const q = this.friendToSearch.trim();
+    if (!q) { this.result = undefined; return; }
+
+    const params = new HttpParams().set('username', q);
+
+    this.http.get<UserSearchResult>('/api/users/search', { params })
+      .subscribe({
+        next: res => this.result = res,
+        error: err => {
+          this.result = undefined;
+          this.error = err.status === 404 ? 'User not found' : 'Server error';
+        }
+      });
+  }
 
 }
