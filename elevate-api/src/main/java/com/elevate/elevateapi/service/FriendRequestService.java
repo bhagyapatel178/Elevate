d9620719ab55170fe6aa76af57dade1208edc2f7
bhagyapatel178 <bhagyapatel178@gmail.com>
2025-07-30 12:16:51 +0100

    chore: friends routing and generated skeleton for friends componenet

---
+++ elevate-api/src/main/java/com/elevate/elevateapi/service/FriendRequestService.java
@@ -0,0 +1,13 @@
+package com.elevate.elevateapi.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FriendRequestService {
+
+    // send friend request (string senderusername, string receiverusername)
+
+    // delete friend request (string senderusername, string receiverusername)
+
+    // list recieved friend requests (string username)
+}
---
+++ elevate-api/src/main/java/com/elevate/elevateapi/service/FriendService.java
@@ -0,0 +1,15 @@
+package com.elevate.elevateapi.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FriendService {
+
+    // accept friend request (String username, String friendusername)
+
+    // remove friend (String username, String friendusername)
+
+    // list friends (String username)
+
+
+}
