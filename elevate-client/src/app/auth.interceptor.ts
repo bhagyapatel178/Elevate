d1340b0edffb10ad897590a955fdc35100b42bf9
bhagyapatel178 <bhagyapatel178@gmail.com>
2025-08-01 18:00:53 +0100

    feat: different url for prod and localhost ng serve

--- elevate-client/src/app/auth.interceptor.ts
+++ elevate-client/src/app/auth.interceptor.ts
@@ -1,6 +1,7 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import {AuthService} from './services/auth.service';
 import {inject} from '@angular/core';
+import {environment} from '../environments/environment';
 
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
@@ -8,12 +9,25 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthService);
   const token = authService.getAuthToken();
 
-  if(token) {
-    req = req.clone({
+  // Prepend the base URL if it's a relative request
+  const isRelativeUrl = !/^http(s)?:\/\//.test(req.url);
+  const updatedUrl = isRelativeUrl ? environment.apiBaseUrl + req.url : req.url;
+
+
+  // Skip auth for login and register
+  const isPublicEndpoint =
+    req.url.includes('/login') || req.url.includes('/register');
+
+  const updatedReq = token && !isPublicEndpoint
+    ? req.clone({
+      url: updatedUrl,
       setHeaders: {
         Authorization: `Bearer ${token}`
       }
     })
-  }
-  return next(req);
+    : req.clone({
+      url: updatedUrl
+    });
+
+  return next(updatedReq);
 };
---
+++ elevate-client/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiBaseUrl: 'https://elevate-v4t5.onrender.com'
+};
