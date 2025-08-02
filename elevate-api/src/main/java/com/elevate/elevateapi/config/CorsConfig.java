8b35fb5320653a16cbd5e1f2e977c010068a289b
bhagyapatel178 <bhagyapatel178@gmail.com>
2025-08-02 09:59:03 +0100

    feat: added a CORS configuration

---
+++ elevate-api/src/main/java/com/elevate/elevateapi/config/CorsConfig.java
@@ -0,0 +1,17 @@
+package com.elevate.elevateapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://elevate-app-bp.onrender.com")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
