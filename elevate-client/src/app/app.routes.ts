bb47da4d8a5c88e9a22dc20ce3f4ea859de4c429
bhagyapatel178 <bhagyapatel178@gmail.com>
2025-08-03 20:59:56 +0100

    feat: landing page and changed routing around

--- elevate-client/src/app/app.routes.ts
+++ elevate-client/src/app/app.routes.ts
@@ -1,11 +1,16 @@
 import { Routes } from '@angular/router';
 import {authGuard} from './auth.guard';
+import {guestGuard} from './unauth.guard';
 
 export const routes: Routes = [
-  { path: '', loadComponent:()=> import('../app/home/home.component').then(m=> m.HomeComponent)},
-  { path: 'register', loadComponent:()=> import('../app/pages/register/register.component').then(m=> m.RegisterComponent)},
-  { path: 'login', loadComponent:()=> import('../app/pages/login/login.component').then(m=> m.LoginComponent)},
+  { path: '', loadComponent:()=> import('../app/pages/landing.component').then(m=> m.LandingComponent), canActivate: [guestGuard]},
+  { path: 'register', loadComponent:()=> import('../app/pages/register/register.component').then(m=> m.RegisterComponent), canActivate: [guestGuard]},
+  { path: 'login', loadComponent:()=> import('../app/pages/login/login.component').then(m=> m.LoginComponent), canActivate: [guestGuard]},
+  { path: 'home', loadComponent:()=> import('../app/home/home.component').then(m=> m.HomeComponent), canActivate: [authGuard]},
   { path: 'profile', loadComponent:()=> import('../app/pages/profile/profile.component').then(m=> m.ProfileComponent), canActivate: [authGuard]},
   { path: 'friends', loadComponent:()=> import('../app/pages/friends/friends.component').then(m=> m.FriendsComponent), canActivate: [authGuard]},
 
+  //nav bar home button routes to root /, so if not logged in, goes to landing, if logged in it routes to root but redirects to /home
+
+  { path: '**', redirectTo:''},
 ];
